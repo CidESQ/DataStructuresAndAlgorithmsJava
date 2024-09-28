b1295e503f43d8d0a1ace53cfb6933ca38aafe78
cidesgo@gmail.com <cidesgo@gmail.com>
2024-09-28 12:10:25 -0600

    Terminando LinkedLists

--- DataStructures/LinkedLists/CircularLinkedList.java
+++ DataStructures/LinkedLists/CircularLinkedList.java
@@ -1,5 +1,182 @@
-package DataStructures.LinkedLists;
 
-public class CircularLinkedList {
+public class CircularLinkedList<T> {
+    /*
+     * boolean contains()
+     */
 
+    private int size;
+    private NodoCircular<T> head;
+    private NodoCircular<T> tail;
+
+    public CircularLinkedList() {
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void addFirst(T data) {
+        NodoCircular<T> newNode = new NodoCircular<>(data, head);
+        if (size == 0) {
+            head = tail = newNode;
+            tail.next = head;
+        } else {
+            newNode.next = head;
+            head = newNode;
+            tail.next = head;
+        }
+        size++;
+    }
+
+    public void addLast(T data) {
+        if (size == 0) {
+            addFirst(data);
+        } else {
+            NodoCircular<T> newNode = new NodoCircular<>(data, head);
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty list");
+        }
+        T data = head.data;
+        if (size == 1) {
+            head = tail = null;
+        } else {
+            head = head.next;
+            tail.next = head;
+        }
+        size--;
+        return data;
+    }
+
+    public T removeLast() {
+        if (isEmpty())
+            throw new RuntimeException("Empty list");
+        T data = tail.data;
+        if (size == 1) {
+            head = tail = null;
+        } else {
+            NodoCircular<T> pointer = head;
+            while (pointer.next != tail) {
+                pointer = pointer.next;
+            }
+            tail = pointer;
+            tail.next = head;
+        }
+        size--;
+        return data;
+    }
+
+    public boolean removeObject(Object o) {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty List");
+        }
+        if (head.data.equals(o)) {
+            if (size == 1) {
+                head = tail = null;
+            } else {
+                head = head.next;
+                tail.next = head;
+            }
+            size--;
+            return true;
+        }
+        NodoCircular<T> pointer = head;
+        while (pointer.next != head) {
+            if (pointer.next.data.equals(o)) {
+                if (pointer.next == tail) {
+                    tail = pointer;
+                }
+                pointer.next = pointer.next.next;
+                size--;
+                return true;
+            }
+            pointer = pointer.next;
+        }
+        return false;
+    }
+
+    public T peekFirst() {
+        if (isEmpty())
+            throw new RuntimeException("Empty list");
+        return head.data;
+    }
+
+    public T peekLast() {
+        if (isEmpty())
+            throw new RuntimeException("Empty list");
+        return tail.data;
+    }
+
+    public boolean contains(Object o) {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty List");
+        }
+        NodoCircular<T> pointer = head;
+        do {
+            if (pointer.data.equals(o)) {
+                return true;
+            }
+            pointer = pointer.next;
+        } while (pointer != head);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedList<Integer> list = new CircularLinkedList<>();
+
+        // Agregar elementos
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addLast(30);
+        list.addLast(40);
+
+        System.out.println("Contenido después de agregar 4 elementos:");
+        System.out.println("Primero: " + list.peekFirst()); // Debería mostrar 20
+        System.out.println("Último: " + list.peekLast()); // Debería mostrar 40
+        System.out.println("Tamaño: " + list.getSize()); // Debería mostrar 4
+
+        // Eliminar el primer elemento
+        System.out.println("\nEliminar el primer elemento: " + list.removeFirst());
+        System.out.println("Nuevo primero: " + list.peekFirst()); // Debería mostrar 10
+
+        // Eliminar el último elemento
+        System.out.println("\nEliminar el último elemento: " + list.removeLast());
+        System.out.println("Nuevo último: " + list.peekLast()); // Debería mostrar 30
+
+        // Verificar si contiene un valor
+        System.out.println("\n¿Contiene 10? " + list.contains(10)); // Debería ser true
+        System.out.println("¿Contiene 40? " + list.contains(40)); // Debería ser false
+
+        // Eliminar un elemento por objeto
+        System.out.println("\nEliminar el 10: " + list.removeObject(10));
+        System.out.println("¿Lista vacía? " + list.isEmpty()); // Debería ser false
+        System.out.println("Nuevo tamaño: " + list.getSize()); // Debería mostrar 1
+
+        // Ver el contenido final
+        System.out.println("\nPrimero: " + list.peekFirst()); // Debería mostrar 30
+    }
+
+}
+
+class NodoCircular<T> {
+    T data;
+    NodoCircular<T> next;
+
+    public NodoCircular(T data, NodoCircular<T> next) {
+        this.data = data;
+        this.next = next;
+    }
 }
--- DataStructures/LinkedLists/DoublyLinkedList.java
+++ DataStructures/LinkedLists/DoublyLinkedList.java
@@ -1,29 +1,14 @@
-package DataStructures.LinkedLists;
 
 public class DoublyLinkedList<T> {
-    /*
-     * TODO:
-     * void clear()
-     * void addFirst(Object o)
-     * void addLast(Object o)
-     * T peekFirst()
-     * T peekLast()
-     * T removeFirst()
-     * T removeLast()
-     * T removeAt(int index)
-     * private remove(NodoDoble<T> nodo)
-     * boolean removeObject(Object o)
-     * int indexOf(Object o)
-     * boolean contains(Object o)
-     * String toString()
-     */
 
     private int size;
     private NodoDoble<T> head;
+    private NodoDoble<T> tail;
 
     public DoublyLinkedList() {
         this.size = 0;
         this.head = null;
+        this.tail = null;
     }
 
     public int getSize() {
@@ -34,19 +19,167 @@ public class DoublyLinkedList<T> {
         return size == 0;
     }
 
-    public void add(T data) {
-        NodoDoble<T> nuevoNodo = new NodoDoble<>(data);
+    public void addFirst(T data) {
         if (isEmpty()) {
-            head = nuevoNodo;
+            head = tail = new NodoDoble(data, null, null);
         } else {
-            NodoDoble<T> pointer = head;
-            while (pointer.next != null) {
-                pointer = pointer.next;
-
-            }
-            pointer.next = nuevoNodo;
+            head.previous = new NodoDoble(data, null, head);
+            head = head.previous;
         }
+        size++;
+    }
 
+    public void addLast(T data) {
+        // NodoDoble<T> nuevoNodo = new NodoDoble<>(data, null, null);
+        // if (isEmpty()) {
+        // head = nuevoNodo;
+        // } else {
+        // NodoDoble<T> pointer = head;
+        // while (pointer.next != null) {
+        // pointer = pointer.next;
+        // }
+        // pointer.next = nuevoNodo;
+        // }
+        if (isEmpty()) {
+            head = tail = new NodoDoble(data, null, null);
+        } else {
+            tail.next = new NodoDoble(data, tail, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    public T peekFirst() {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty list");
+        }
+        return head.data;
+    }
+
+    public T peekLast() {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty list");
+        }
+        return tail.data;
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty List");
+        }
+        T extractedData = head.data;
+        head = head.next;
+        size--;
+        if (isEmpty()) {
+            tail = null;
+        } else {
+            head.previous = null;
+        }
+        return extractedData;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty List");
+        }
+        T extractedData = tail.data;
+        tail = tail.previous;
+        size--;
+        if (isEmpty()) {
+            head = null;
+        } else {
+            tail.next = null;
+        }
+        return extractedData;
+    }
+
+    private T remove(NodoDoble<T> nodo) {
+        if (nodo.previous == null) {
+            return removeFirst();
+        }
+        if (nodo.next == null) {
+            return removeLast();
+        }
+        T data = nodo.data;
+        nodo.next.previous = nodo.previous;
+        nodo.previous.next = nodo.next;
+        nodo.data = null;
+        nodo = nodo.previous = nodo.next = null;
+        size--;
+        return data;
+    }
+
+    public T removeAt(int index) {
+        if (index < 0 || index >= size || isEmpty()) {
+            throw new RuntimeException("Empty list or index out of bounds");
+        }
+        int i;
+        NodoDoble<T> pointer;
+        // Search from the begining of the list
+        for (i = 0, pointer = head; i != index; i++) {
+            pointer = pointer.next;
+        }
+        size--;
+        return remove(pointer);
+    }
+
+    public boolean removeObject(Object o) {
+        if (isEmpty()) {
+            throw new RuntimeException("Empty list");
+        }
+        int i = 0;
+        NodoDoble<T> finder = head;
+        while (i < size) {
+            if (finder.data.equals(o)) {
+                removeAt(i);
+                return true;
+            }
+            finder = finder.next;
+            i++;
+        }
+        return false;
+    }
+
+    public int indexOf(Object o) {
+        NodoDoble<T> pointer = head;
+        int index = 0;
+        while (pointer != null) {
+            if (o.equals(pointer.data)) {
+                return index;
+            }
+            pointer = pointer.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    public void clear() {
+        NodoDoble<T> pointer = head;
+        while (pointer != null) {
+            NodoDoble<T> nextNode = pointer.next;
+            pointer.previous = pointer.next = null;
+            pointer.data = null;
+            pointer = nextNode;
+        }
+        head = tail = pointer = null;
+        size = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        NodoDoble<T> pointer = head;
+        while (pointer != null) {
+            sb.append(pointer.data + ", ");
+            pointer = pointer.next;
+        }
+        sb.append(" ]");
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -59,9 +192,9 @@ class NodoDoble<T> {
     NodoDoble<T> next;
     T data;
 
-    public NodoDoble(T data) {
+    public NodoDoble(T data, NodoDoble<T> previous, NodoDoble<T> next) {
         this.data = data;
-        this.previous = null;
-        this.next = null;
+        this.previous = previous;
+        this.next = next;
     }
 }
